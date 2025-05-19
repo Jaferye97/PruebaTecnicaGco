d0ed5e988b9185f44be5ba0aef7dc53925904291
YeimerJaramillo <andresjara0897@hotmail.com>
2025-05-19 18:08:56 -0500

    Backend: Se agrega proyecto backend Java base

---
+++ Backend/src/main/java/com/yeimer/ecommerceapi/EcommerceGcoApplication.java
@@ -0,0 +1,13 @@
+package com.yeimer.ecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGcoApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/yeimer/ecommerceapi/EcommerceGcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeimer.ecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceGcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
