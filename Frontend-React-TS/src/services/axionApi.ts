0951f13f69d522af9e5d8e4700ccf8a98e07a13c
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-05-27 05:34:49 -0500

    Frontend React-Ts: Ajuste de servicio de consulta de API

--- Frontend-React-TS/src/services/axionApi.ts
+++ Frontend-React-TS/src/services/axionApi.ts
@@ -25,7 +25,6 @@ export const requestData = async <T>(
 
       if (errorApi.error == "Internal Server Error") {
         Swal.fire({
-          position: "top-end",
           icon: "warning",
           title: errorApi.message || messageError,
           showConfirmButton: false,
@@ -36,7 +35,6 @@ export const requestData = async <T>(
       if (errorApi.error == "Validation Error") {
         console.log(errorApi);
         Swal.fire({
-          position: "top-end",
           icon: "warning",
           title: "Validation Error",
           showConfirmButton: false,
